7836ff80215573ab59013931480d93cf7220c399
Samuel Pena <ss.pena@uniandes.edu.co>
2024-05-29 00:25:18 -0500

    hggh

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SamuelSantiago'`, () => {
+  it(`should have as title 'myWeb'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SamuelSantiago');
+    expect(app.title).toEqual('myWeb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SamuelSantiago');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, myWeb');
   });
 });
